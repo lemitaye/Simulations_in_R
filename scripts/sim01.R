844890ba9c5a492030f6f5062ba11f41da7f62d5
Lemi Taye <tayelemi@gmail.com>
2020-08-08 18:57:22 -0600

    Summarize and plot the simulation.

--- scripts/sim01.R
+++ scripts/sim01.R
@@ -13,3 +13,18 @@ for (i in 1:r) {
         # calculate the ratio of heads and save in 'prob'
         prob[i] <- sum(x)/10000
 }
+
+# Some summary and an interactive plot
+summary(prob)
+
+ggplot() +
+        geom_line(aes(seq_along(prob), prob)) +
+        geom_hline(aes(yintercept = 0.5), color = "red", size = 1) +
+        coord_cartesian(ylim = c(0.47,0.53)) +
+        labs(x ="Number of Trials (n)",
+             y = "Probability")
+# save the plot
+ggsave("figures/prob_freq.png", width = 7.77, height = 4.29)
+
+
+
