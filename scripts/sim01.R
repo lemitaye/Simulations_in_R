0e77c7e56efc681e0717236a3c8a09c1530d68bc
Lemi Taye <tayelemi@gmail.com>
2020-08-08 18:43:34 -0600

    Run a for loop to simulate a coin toss.

--- scripts/sim01.R
+++ scripts/sim01.R
@@ -5,3 +5,11 @@ library(ggplot2)
 # Frequentist Interpretation of probability
 r <- 1000
 prob <- numeric(r)
+
+# Run a 'for' loop:
+for (i in 1:r) {
+        # toss a fair coin 10000 times
+        x <- rbinom(n = 10000, size = 1, prob = 0.5)
+        # calculate the ratio of heads and save in 'prob'
+        prob[i] <- sum(x)/10000
+}
